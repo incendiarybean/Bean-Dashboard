81b4064f8c0672eff45fb60b096e9a50d56b2b34
incendiarybean <ben.weare12@gmail.com>
2021-03-11 14:45:55 +0000

    Made Note use unified React loader component

--- src/components/home/addons/sticky/index.jsx
+++ src/components/home/addons/sticky/index.jsx
@@ -3,29 +3,10 @@ import { Note } from './addon';
 
 function Component(props){
 
-    const Loader = (props) => {
-        return (
-            <div className='hidden lg:flex shadow-inner bg-other w-full p-2 justify-center items-center'>
-                <div  className={`-mt-20 animate__animated animate__flash animate__infinite animate__slower shadow-lg absolute w-96 rounded-lg border border-${props.color}-400`}>
-                    <div className={`note relative rounded-t-lg w-full p-1 flex justify-between border-b border-${props.color}-400`}>
-                        <p className={`font-semi-bold leading-wide px-2 text-${props.color}-400`}>{props.text}</p>
-                        <svg height='21' viewBox='0 0 21 21' width='21' xmlns='http://www.w3.org/2000/svg'><circle className={`stroke-current text-${props.color}-400`} cx='10.5' cy='10.5' fill='none' r='8' stroke='#2a2e3b' strokeLinecap='round' strokeLinejoin='round'/></svg>
-                    </div>
-                    <div id='note_content' className='h-56 note p-2'>
-                        <div className={`p-2 bg-${props.color}-400 rounded mt-4`}></div>
-                        <div className={`p-2 bg-${props.color}-400 rounded mt-4 w-2/3`}></div>
-                        <div className={`p-2 bg-${props.color}-400 rounded mt-4`}></div>
-                        <div className={`p-2 bg-${props.color}-400 rounded mt-4 w-2/3`}></div>
-                    </div>
-                </div>
-            </div>
-        );
-    };
-
     if(!props.Notes.Loaded){
-        return <Loader {...{color:'purple', text:'Loading...'}}/>;
+        return <props.Loader {...{color:'purple', text:'Loading...'}}/>;
     } else if(props.Notes.Loaded === 'Failed') {
-        return <Loader {...{color:'red', text:'Failed...'}}/>;
+        return <props.Loader {...{color:'red', text:'Failed...'}}/>;
     } else {
         return (
             <div style={{ backgroundImage: `url('${props.Theme.background}')` }} id='notes_container' onDoubleClick={(e) => props.Notes.createNote(e)} className='notes-container animate__animated animate__fadeIn relative bg-blue-500 shadow-inner hidden lg:block w-full h-full'>
