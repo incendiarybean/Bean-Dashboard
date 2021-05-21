bb07933db5d907037d27c9b66ac6de5c82e20ded
incendiarybean <ben.weare12@gmail.com>
2021-05-21 14:29:50 +0100

    Testing ARCH

--- src/components/home/addons/news/addon/article.jsx
+++ src/components/home/addons/news/addon/article.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Component(props){
 
     return (
-        <a className='min-w-100 group transition duration-300 ease-in-out bg-white dark:bg-coolGray-700 article relative shadow cursor-pointer bg-white dark:bg-coolGray-700 hover:bg-blue-100 dark:hover:bg-coolGray-800 overflow-hidden border-b-4 border-accent w-full mt-4 xl:mt-0 xl:w-1/3 mr-0 md:mr-4' href={props.data.link}>
+        <a className='group transition duration-300 ease-in-out bg-white dark:bg-coolGray-700 article relative shadow cursor-pointer bg-white dark:bg-coolGray-700 hover:bg-blue-100 dark:hover:bg-coolGray-800 overflow-hidden border-b-4 border-accent w-full mt-4 xl:mt-0 xl:w-1/3 mr-0 md:mr-4' href={props.data.link}>
             <img src={props.data.img} alt='People' className='transition duration-300 ease-in-out group-hover:opacity-75 w-full h-full object-cover h-32'/>
             <div className='h-full absolute bottom-0 z-50 bg-coolGray-700 bg-opacity-75 w-full flex flex-col'>
                 <div className='shadow-inner p-4 min-h-24 z-10 overflow-none'>
--- src/components/home/addons/sticky/index.jsx
+++ src/components/home/addons/sticky/index.jsx
@@ -9,15 +9,15 @@ function Component(props){
         return <props.Loader {...{color:'red', text:'Failed...'}}/>;
     } else {
         return (
-            <div style={{ backgroundImage: `url('${props.Theme.background}')` }} id='notes_container' onDoubleClick={(e) => props.Notes.createNote(e)} className='bg-coolGray-200 dark:bg-coolGray-700 animate__animated animate__fadeIn notes-container relative bg-blue-500 shadow-inner hidden lg:block w-full h-full'>
+            <div id='notes_container' onDoubleClick={(e) => props.Notes.createNote(e)} className='bg-coolGray-200 dark:bg-coolGray-700 animate__animated animate__fadeIn notes-container relative bg-blue-500 shadow-inner hidden lg:block w-full h-full'>
                 {props.Notes.Note.map((data) => (
                     <Note key={data._id} {...props} data={data} />
                 ))}
-                    <div className="animate__animated animate__fadeIn absolute p-1 transform scale-125 bg-coolGray-100 dark:bg-coolGray-700 rounded-full shadow m-3" onClick={e => props.Notes.createNote({ target: document.getElementById('notes_container') , clientX: 800, clientY:150 })}>
-                        <svg className='transform scale-125 cursor-pointer text-accent hover:text-blue-500 animate__animated animate__fadeIn' height="21" viewBox="0 0 21 21" width="21" xmlns="http://www.w3.org/2000/svg">
-                            <g className="stroke-current" fill="none" fillRule="evenodd" stroke="#9f7aea" strokeLinecap="round" strokeLinejoin="round" transform="translate(4 4)"><path d="m10.5.5h-8c-1.1045695 0-2 .8954305-2 2v8c0 1.1045695.8954305 2 2 2h8c1.1045695 0 2-.8954305 2-2v-8c0-1.1045695-.8954305-2-2-2z" transform="matrix(0 1 -1 0 13 0)"/><path d="m6.5 3.5v6.056"/><path d="m6.5 3.5v6" transform="matrix(0 1 -1 0 13 0)"/></g>
-                        </svg>
-                    </div>
+                <div className="animate__animated animate__fadeIn absolute p-1 transform scale-125 rounded-full m-3" onClick={e => props.Notes.createNote({ target: document.getElementById('notes_container') , clientX: 800, clientY:150 })}>
+                    <svg className='transform scale-125 cursor-pointer text-accent hover:text-blue-500 animate__animated animate__fadeIn' height="21" viewBox="0 0 21 21" width="21" xmlns="http://www.w3.org/2000/svg">
+                        <g className="stroke-current" fill="none" fillRule="evenodd" stroke="#9f7aea" strokeLinecap="round" strokeLinejoin="round" transform="translate(4 4)"><path d="m10.5.5h-8c-1.1045695 0-2 .8954305-2 2v8c0 1.1045695.8954305 2 2 2h8c1.1045695 0 2-.8954305 2-2v-8c0-1.1045695-.8954305-2-2-2z" transform="matrix(0 1 -1 0 13 0)"/><path d="m6.5 3.5v6.056"/><path d="m6.5 3.5v6" transform="matrix(0 1 -1 0 13 0)"/></g>
+                    </svg>
+                </div>
             </div>
         );
     }
